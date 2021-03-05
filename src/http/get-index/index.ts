35c3a67cbcd2e19e4a36e952ad1a44a5aeb7091f
zaverden <zaverden@gmail.com>
2021-03-05 21:58:26 +0700

    test multy cookies response

--- src/http/get-index/index.ts
+++ src/http/get-index/index.ts
@@ -5,5 +5,9 @@ export async function handler(req: object): Promise<HttpFunctionResponse> {
   return {
     statusCode: 200,
     body: JSON.stringify({ ok: true, date: getData(""), req }),
+    headers: {
+      "set-cookie": "z=zxc",
+    },
+    cookies: ["q=qwe", "a=asd"],
   };
 }
